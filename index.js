de9c896dd0dd3dc2bbe2b562921a9646cf4a8f1d
Cody Green <C.Green@F5.com>
2018-07-16 13:11:02 -0500

    added simple express redirect site as placeholder

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-const r = require('./ravelloClasses');
+const express = require('express');
+const RavelloClass = require('./ravelloClasses');
 
+const app = express()
+
+app.get('/', (req, res) => res.send('Agility 2018'))
+app.get('/*', (req, res) => {
+    const classes = new RavelloClass({
+        domain: process.env.DOMAIN,
+        password: process.env.PASSWORD,
+        username: process.env.USERNAME,
+    });
+
+    classes.getCache(req.url.substring(1))
+    .then((link) => {
+        if(link === null) {
+            res.send('Bad class and/or student ID');
+        } else {
+            res.redirect(link);
+        }
+    })
+    .catch((err) => {
+        throw err;
+    });
+})
+
+app.listen(3000, () => console.log('Agility Portal Redirect listening on port 3000!'))
